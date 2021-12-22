e1e475a9c870a52bb0943861e8bace4454817f99
ShautPh <sauth.phouek168@gmail.com>
2021-12-22 08:41:53 +0700

    start the coding

--- invader_shooter.py
+++ invader_shooter.py
@@ -1,1 +1,11 @@
-#Welcome to my game
+import tkinter as tk
+import random
+import time 
+
+window = tk.Tk()
+window.geometry("1400x700")
+window.title("Space Invader by (Sauth and MengYi)")
+
+
+
+window.mainloop()
