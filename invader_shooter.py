e4ff67070b26ea8393218e2a845a0477ca618ec2
ShautPh <sauth.phouek168@gmail.com>
2021-12-22 11:17:55 +0700

    add some pic

--- invader_shooter.py
+++ invader_shooter.py
@@ -22,7 +22,11 @@ player_pos = canvas.create_image(300, 400, image=player)
 
 # Enemies image.....................
 enemy_1 = tk.PhotoImage(file="./img/black-animy.png")
-black_enemy = canvas.create_image(1100, 400, image=enemy_1) 
+enemy_2 = tk.PhotoImage(file="./img/blue-animy.png")
+enemy_3 = tk.PhotoImage(file="./img/red-animy.png")
+# black_enemy = canvas.create_image(1100, 20, image=enemy_1) 
+# blue_enemy = canvas.create_image(1100, 20, image=enemy_2) 
+# red_enemy = canvas.create_image(1100, 20, image=enemy_3) 
 
 #Move Up(player) 
 def goUp(event):
@@ -54,18 +58,29 @@ def goRight(event):
             canvas.move(player_pos,2,0)
         time.sleep(0.01)
 
-def enemy_coming():
-    while True: 
+def enemy1_coming():
+    black_enemy = canvas.create_image(1100, 0, image=enemy_1) 
+    while not False: 
         canvas.update()
-        if canvas.coords(black_enemy)[0] > 700:
-            canvas.move(black_enemy,-1,0)
-        elif canvas.coords(black_enemy)[1] > 50 and canvas.coords(black_enemy)[0]== 700:
-            canvas.move(black_enemy,0,-1)
-        elif canvas.coords(black_enemy)[1] > 50 and canvas.coords(black_enemy)[0] < 700:
-            canvas.move(black_enemy,0,1)
-        time.sleep(0.001)
+        canvas.move(black_enemy,-1,1)
+        time.sleep(0.01)
+def enemy2_coming():
+    blue_enemy = canvas.create_image(1100, 0, image=enemy_2) 
+    while not False: 
+        canvas.update()
+        canvas.move(blue_enemy,-1,1)
+        time.sleep(0.01)
+def enemy3_coming():
+    red_enemy = canvas.create_image(1100, 0, image=enemy_3) 
+    while not False: 
+        canvas.update()
+        canvas.move(red_enemy,-1,1)
+        time.sleep(0.01)
+random
 
-canvas.after(200,enemy_coming)
+canvas.after(1000,enemy1_coming)
+canvas.after(900,enemy2_coming)
+canvas.after(800,enemy3_coming)
 #Button to controll the player
 window.bind("<w>",goUp)
 window.bind("<s>",goDown)
