dd370d41b64453eb6fd171cf924092805b37a736
Student <mengyi.yoeng34@gmail.com>
2021-12-22 15:39:48 +0700

    moval random

--- invader_shooter.py
+++ invader_shooter.py
@@ -21,13 +21,14 @@ player = tk.PhotoImage(file="./img/player.png")
 player_pos = canvas.create_image(300, 400, image=player)
 
 # Enemies image.....................
-enemy = ['black-animy','blue-animy','red-animy']
-enemy_1 = tk.PhotoImage(file="./img/"+ random.choice(enemy)+".png")
-# enemy_2 = tk.PhotoImage(file="./img/blue-animy.png")
-# enemy_3 = tk.PhotoImage(file="./img/red-animy.png")
-black_enemy = canvas.create_image(1100, 20, image=enemy_1) 
-# blue_enemy = canvas.create_image(1100, 20, image=enemy_2) 
-# red_enemy = canvas.create_image(1100, 20, image=enemy_3) 
+enemy_1 = tk.PhotoImage(file="./img/black-animy.png")
+enemy_2 = tk.PhotoImage(file="./img/blue-animy.png")
+enemy_3 = tk.PhotoImage(file="./img/red-animy.png")
+enemies = [enemy_1,enemy_2,enemy_3]
+black_enemy = canvas.create_image(1100, 300, image=random.choice(enemies)) 
+
+# Variable
+amountOfEnemies = 2
 
 
 
@@ -62,15 +63,17 @@ def goRight(event):
         time.sleep(0.01)
 
 def enemy1_coming():
-    global black_enemy
+    global black_enemy, amountOfEnemies
     position = random.randrange(25, 600)
     pos = canvas.coords(black_enemy)
-    if pos[0] > 550 and pos[1] < 600:
-        canvas.move(black_enemy, -10.15,0)
-        canvas.after(150, enemy1_coming)
-    else:
-        black_enemy = canvas.create_image(1100, position, image=enemy_1) 
-    canvas.after(150, enemy1_coming)
+    positionOfenemy = random.randrange(500, 800)
+    if amountOfEnemies <= 10:
+        if pos[0] > positionOfenemy and pos[1] < 600:
+            canvas.move(black_enemy, -10.15,0)
+        else:
+            black_enemy = canvas.create_image(1100, position, image=random.choice(enemies)) 
+            amountOfEnemies += 1
+    canvas.after(100, enemy1_coming)
 
     # canvas.after(100,enemy1_coming)
 
