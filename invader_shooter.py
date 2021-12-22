b96da25ca8f104450fa946922b8a95b8d82e28f7
Student <mengyi.yoeng34@gmail.com>
2021-12-22 09:59:36 +0700

    player image

--- invader_shooter.py
+++ invader_shooter.py
@@ -13,15 +13,26 @@ window.geometry("1400x700")
 frame = tk.Frame()
 window.title("Space Invader by (Sauth and MengYi)")
 canvas = tk.Canvas(frame)
-player_pos = canvas.create_rectangle(300,300,400,350, fill="red")
+
+
+# Add Image
+player = tk.PhotoImage(file="./img/player.png")
+player_pos = canvas.create_image(300, 400, image=player)
+
 
 #Move Up 
 #Move down 
 #Move left 
+def goLeft(event):
+    canvas.move(player_pos, -20, 0)
 #Move right
+def goRight(event):
+    canvas.move(player_pos, 20, 0)
 
 #Button to controll the player
-canvas.pack("<w>",goUp)
+# canvas.pack("<w>",goUp)
+window.bind("<d>",goRight)
+window.bind("<a>",goLeft)
 #display window
 canvas.pack(expand=True,fill="both")
 frame.pack(expand=True,fill="both")
