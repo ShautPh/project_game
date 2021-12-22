1739fa7a0fb0507b066ad4f441928f98618cbde7
ShautPh <sauth.phouek168@gmail.com>
2021-12-22 08:43:54 +0700

    start the code

--- invader_shooter.py
+++ invader_shooter.py
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+# Welcome to my game
+# Group 24 - Mengyi & Sauth
 import tkinter as tk
 import random
 import time 
@@ -10,7 +11,4 @@ window.title("Space Invader by (Sauth and MengYi)")
 
 
 window.mainloop()
-=======
-# Welcome to my game
-# Group 24 - Mengyi & Sauth
->>>>>>> 357798fc165f86195b6bb74f03f560742211fbad
+
