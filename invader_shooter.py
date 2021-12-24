c69518dabb39f0d82046c6588c7fc2a4fe3cbbcd
ShautPh <sauth.phouek168@gmail.com>
2021-12-24 09:21:25 +0700

    add background

--- invader_shooter.py
+++ invader_shooter.py
@@ -3,7 +3,10 @@
 import tkinter as tk
 import random
 import time
-from tkinter.constants import ANCHOR, SE 
+from tkinter import font
+from tkinter.constants import ANCHOR, COMMAND, NW, SE
+from tkinter.font import BOLD 
+import winsound
 
 
 # ----------------------------------------------
@@ -12,6 +15,7 @@ from tkinter.constants import ANCHOR, SE
 window = tk.Tk()
 #Adjust size of the window
 window.geometry("1200x650")
+window.resizable(False,False)
 
 #The title of the window
 frame = tk.Frame()
@@ -22,51 +26,74 @@ canvas = tk.Canvas(frame)
 bg = tk.PhotoImage(file="./img/start-game.png")
 bg_game = tk.PhotoImage(file="./img/battle-game.png")
 
-# PLAYER IMAGE........................
+# # PLAYER IMAGE........................
 player = tk.PhotoImage(file="./img/player.png")
-
-# Background image
-# label1 = tk.Label(window, image=bg)
-# label1.place(x=0, y=0)
-
-battle_image = canvas.create_image(1200, 650, anchor=SE, image=bg_game) 
-
-# Enemies image.....................
-
+# ENNEMY IMAGES.....................
 black_ennemy_image = tk.PhotoImage(file="./img/black-animy.png")
 blue_ennemy_image = tk.PhotoImage(file="./img/blue-animy.png")
 red_ennemy_image = tk.PhotoImage(file="./img/red-animy.png")
 
+display_game = False
+def display_start_game():
+    global display_game
+    if not display_game: 
+        canvas.create_image(0,0,anchor=NW, image = bg,tags="start")
+        canvas.create_rectangle(378,290,534,350,fill="red",outline="",tags="start")
+        canvas.create_text(454,320,text="START",font=("Purisa", 30, BOLD), fill="white",tags=("startTheGame","start"))
+        canvas.create_rectangle(660,290,816,350,fill="red",outline="",tags="start")
+        canvas.create_text(744,320,text="EXIT",font=("Purisa", 30, BOLD), fill="white",tags=("exitTheGame","start"))
+    # winsound.PlaySound("sound/explosion.wav",winsound.SND_FILENAME | winsound.SND_ALIAS)
+display_start_game()
 
-# ----------------------------------------------
-# CONSTANTS
-# ----------------------------------------------
+#EXIT THE WINDOW TO STOP THE PROGRAME-----------------------------
+def close_the_window(event):
+    window.destroy()
+
+def start_process(event):
+    global display_game
+    canvas.delete("start")
+    display_game = True
+    if display_game: 
+        canvas.after(100,loading_the_process)
+
+def loading_the_process():
+    canvas.create_text(600,300,text="Loading...", font= ("Purisa", 40,BOLD), fill="red")
+    canvas.after(random.randrange(1000,6000),in_processing)
+
+def in_processing():
+    global player_pos
+    battle_image = canvas.create_image(1200, 650, anchor=SE, image=bg_game)
+    # CALL THE FUNCTION TO P/ROGRESS=========================================
+    canvas.after(500,create_enemy)
+    canvas.after(500,move_enemies)
+    player_pos = canvas.create_image(300, 400, image=player)    
+# # ----------------------------------------------
+# # CONSTANTS
+# # ----------------------------------------------
 MOVE_PLAYER_INCREMENT = 16
 ENNEMY_IMAGES = [black_ennemy_image,blue_ennemy_image,red_ennemy_image]
 
-
-# ----------------------------------------------
-# VARIABLES
-# ----------------------------------------------
+# # ----------------------------------------------
+# # VARIABLES
+# # ----------------------------------------------
 listOfEnemies = []
-player_pos = canvas.create_image(300, 400, image=player)
+# player_pos = canvas.create_image(300, 400, image=player)
 
 newEnnemyStartX = 1200
 newEnnemyStartY = 30
 
-
-# THE POSITION OF THE PLAYER===================
+# # THE POSITION OF THE PLAYER================================================
 def getPlayerPosition():
     return canvas.coords(player_pos)
 
 # MOVE POSITION PLAYER BY USING KEY PRESS=====================================
-def onWPressed(event) :
+def onWPressed(event):
     goUp()
-def onSPressed(event) :
+def onSPressed(event):
     goDown()
-def onAPressed(event) :
+def onAPressed(event):
     goLeft()
-def onDPressed(event) :
+def onDPressed(event):
     goRight()
 
 #MOVE PLAYER UP ==========================================================
@@ -76,15 +103,15 @@ def goUp():
 
 #MOVE PLAYER DOWN ======================================================
 def goDown():
-    if canvas.coords(player_pos)[1] < 600:
+    if getPlayerPosition()[1] < 600:
         canvas.move(player_pos,0,MOVE_PLAYER_INCREMENT)
 #MOVE PLAYER TO LEFT======================================================== 
 def goLeft():
-    if canvas.coords(player_pos)[0] > 20:
+    if getPlayerPosition()[0] > 20:
         canvas.move(player_pos,-MOVE_PLAYER_INCREMENT,0)
 #MOVE PLAYER TO RIGHT======================================================
 def goRight():
-    if canvas.coords(player_pos)[0] < 1000:
+    if getPlayerPosition()[0] < 1000:
         canvas.move(player_pos,MOVE_PLAYER_INCREMENT,0)
 
 
@@ -93,7 +120,7 @@ def goRight():
 # CREATE THE ENNEMIES TO DISPLAY ON SCREEN ===================
 def create_enemy():
     global newEnnemyStartY
-    if len(listOfEnemies) < 6  :
+    if len(listOfEnemies) < 6 :
         newEnnemyStartY =random.randrange(40,500)
         ennemyImage = random.choice(ENNEMY_IMAGES)
         newEnemy = canvas.create_image(newEnnemyStartX,newEnnemyStartY,image=ennemyImage, tags="all_enemy")
@@ -109,24 +136,22 @@ def move_enemies():
     for enemy in listOfEnemies:
         canvas.move(enemy, -10, 0)
         position = canvas.coords(enemy)
-        if position[0] < 50:
+        if position[0] < 20:
             ennemiesToBeDeleted.append(enemy)
     for ennemy in ennemiesToBeDeleted:
         listOfEnemies.remove(ennemy)
         canvas.delete(ennemy)
     canvas.after(100,move_enemies)
 
-#CALL THE FUNCTION TO PROGRESS=========================================
-create_enemy()
-move_enemies()
-
-
-#KEYS THAT PLAYER HAS TO PRESS TO PLAY THE GAME=================================
+# KEYS THAT PLAYER HAS TO PRESS TO PLAY THE GAME=================================
 window.bind("<w>", onWPressed)
 window.bind("<s>",onSPressed)
 window.bind("<d>",onDPressed)
 window.bind("<a>",onAPressed)
 
+#LEFT CLICK TO START OR EXIT THE GAME==================================
+canvas.tag_bind("startTheGame","<Button-1>",start_process)
+canvas.tag_bind("exitTheGame","<Button-1>",close_the_window)
 #DISPLAY WINDOW====================================================
 canvas.pack(expand=True,fill="both")
 frame.pack(expand=True,fill="both")
